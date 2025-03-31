6bc0cf84342ebf2ff8075ae129041a412710efc0
Phamela Mhlaba <phamelamhlaba@gmail.com>
2025-03-31 20:45:10 +0200

    Updated the way I import so that I use ES Modules

---
+++ postcss.config.mjs
@@ -0,0 +1,14 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import presetEnv from "postcss-preset-env"; 
+
+export default {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    presetEnv({
+      stage: 3,
+      features: { "custom-properties": false },
+    }),
+  ],
+};
